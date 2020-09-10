d56f2f1eb784572b10672462fe77aadc57e3f15e
Oleg Chulkov <pilot114@bk.ru>
2020-09-11 00:51:37 +0700

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,37 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="grid">
+      <div>
+        <Graph/>
+      </div>
+      <!-- <div>
+        <Graph/>
+      </div>
+      <div>
+        <Graph/>
+      </div>
+      <div>
+        <Graph/>
+      </div> -->
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+  import Graph from './components/Graph'
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  export default {
+    name: 'App',
+    components: {
+      Graph
+    },
   }
-}
 </script>
-
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+#grid { 
+  display: grid;
+  grid-template-rows: 1fr;
+  grid-template-columns: 1fr;
+}
+#grid div {
+  border: 1px solid;
 }
 </style>
