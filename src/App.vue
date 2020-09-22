9eec386d81bc57ee4bf84f9bba83432b6bddbaba
Oleg Chulkov <pilot114@bk.ru>
2020-09-23 01:18:48 +0700

    Graph update

--- src/App.vue
+++ src/App.vue
@@ -1,45 +1,46 @@
 <template>
-  <div id="grid">
-      <div>
-        <Graph
-          :isDirected="false"
-          :nodes="new Array(100).fill(0).map((x, i) => {
-              return {
-                  id: i,
-                  group: Math.round(Math.random()*9),
-                  name: i,
-                }
-            })"
-          :links="new Array(100).fill(0).map((x, i) => {
-              return {
-                source: i,
-                target: i > 50 ? i-1 : i+1,
-                weight: Math.round(Math.random()*10)
-              }
-            })"
-          :h="800"
-        />
-      </div>
-  </div>
+    <div id="grid">
+        <div>
+            <Graph
+                :isDirected="false"
+                :nodes="new Array(100).fill(0).map((x, i) => {
+                    return {
+                        id: i,
+                        group: Math.round(Math.random()*9),
+                        name: i,
+                    }
+                })"
+                :links="new Array(100).fill(0).map((x, i) => {
+                    return {
+                        source: i,
+                        target: i > 50 ? i-1 : i+1,
+                        weight: Math.round(Math.random()*10)
+                    }
+                })"
+                :h="800"
+            />
+        </div>
+    </div>
 </template>
 
 <script>
-  import Graph from './components/Graph'
+import Graph from './components/Graph'
 
-  export default {
+export default {
     name: 'App',
     components: {
-      Graph
+        Graph
     },
-  }
+}
 </script>
 <style>
-#grid { 
-  display: grid;
-  grid-template-rows: 1fr;
-  grid-template-columns: 1fr;
+#grid {
+    display: grid;
+    grid-template-rows: 1fr;
+    grid-template-columns: 1fr;
 }
+
 #grid div {
-  border: 1px solid;
+    border: 1px solid;
 }
 </style>
